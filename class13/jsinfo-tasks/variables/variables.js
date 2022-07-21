ddab25977476099dcce66e10a66d76521dcde03b
siriusscaper <siriusscaper@gmail.com>
2022-07-21 14:00:10 -0700

    changes

---
+++ class13/jsinfo-tasks/variables/variables.js
@@ -0,0 +1,31 @@
+// Working with variables
+// importance: 2
+
+// Declare two variables: admin and name.
+let admin
+
+let name
+// Assign the value "John" to name.
+name = "John"
+
+// Copy the value from name to admin.
+admin = name
+
+// Show the value of admin using alert (must output “John”).
+alert(admin)
+
+
+// Giving the right name
+// importance: 3
+
+// Create a variable with the name of our planet. How would you name such a variable?
+const ourPlanet = "Earth"
+
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentUserName = "Ryan"
+
+// Uppercase const?
+// importance: 4
+
+// Would it be right to use upper case for birthday? For age? Or even for both?
+// No, given the definition in the article using uppercase is reserved for values that are known beforehand. Deriving age from a birthdate wouldn't qualify, but the birthday might since it's not going to change and could be hardcoded.
