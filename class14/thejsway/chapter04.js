d6ba4b274b3cd86d7d1e75a0d73ec2abf48f73d3
siriusscaper <siriusscaper@gmail.com>
2022-08-07 13:19:10 -0700

    finished thejsway chapter4 challenges

--- class14/thejsway/chapter04.js
+++ class14/thejsway/chapter04.js
@@ -106,17 +106,18 @@ while (num4 > 9 || num4 < 2) {
 // Neither yes nor no
 // Write a program that plays "neither yes, nor no" with the user. Specifically, the programs asks the user to enter text until either "yes" or "no" is typed, which ends the game.
 
-let text = prompt()
+let text = prompt("Neither yes, nor no")
 
-while (text != "no" || text != "yes"){
+while (text !== "no" && text !== "yes"){
   text = prompt("Neither yes, nor no")
+  console.log(text);
 }
 
 // FizzBuzz
 // Write a program that shows all numbers between 1 and 100 with the following exceptions:
-for (i = 1; i < 100; i++) {
+for (i = 1; i < 101; i++) {
   // When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
-  if (i % 3 === 0 && i % 5 === 0){
+  if (i % 15 === 0){
     console.log("FizzBuzz");
   } else if (i % 5 === 0) {
     // It shows "Buzz" instead if the number is divisible by 5 and not by 3.
