933e78d0e8dba7b7216d010233c43ffa4813888b
siriusscaper <siriusscaper@gmail.com>
2022-09-20 22:04:12 -0700

    starting up a react app using create react app

---
+++ class57/react-task-tracker/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From React</h1>
+      <h2>Hello</h2>
+      <h3>Test</h3>
+    </div>
+  );
+}
+
+export default App;
