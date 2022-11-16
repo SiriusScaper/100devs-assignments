620c99abbc9ff3cf5646d1788eff07e1d4284004
siriusscaper <siriusscaper@gmail.com>
2022-11-15 21:17:47 -0800

    Practice

--- class14/thejsway/chapter04.js
+++ class14/thejsway/chapter04.js
@@ -9,3 +9,14 @@ for (let t = 1; t <= 10; t++) {
   console.log(t);
 }
 
+let userPrompt = prompt()
+
+while (userPrompt > 0) {
+  userPrompt--
+  console.log(userPrompt);
+}
+
+
+for (let userPrompt2 = prompt(); userPrompt2 > 0; userPrompt2--) {
+  console.log(userPrompt2)
+}
